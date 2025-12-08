e8f75e544c24c7a4821b71de19e75894992b117e
narh123 <narh2001@gmail.com>
2025-12-08 19:35:44 +0300

    added all file

---
+++ j.s/8-12-25/app.js
@@ -0,0 +1,11 @@
+// var firstRamzan = new Date('18 feb 2025');
+
+// var today = new Date();
+
+// var diff = firstRamzan.getTime() - today.getTime();
+// console.log(diff);
+
+// console.log(firstRamzan.getTime());
+// console.log(today.getTime());
+
+
